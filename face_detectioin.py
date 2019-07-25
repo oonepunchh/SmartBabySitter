5643c24c7c71c661ef012b7f3e90c9ba822b32c3
doooomdoom <dhdh3311@naver.com>
2019-07-25 16:23:48 +0900

    This code is about body detecting......
    I wanna be...a...... billionaire......,...........

--- face_detectioin.py
+++ face_detectioin.py
@@ -22,9 +22,9 @@ FACIAL_LANDMARKS_INDEXES = OrderedDict([
 
 def shape_to_numpy_array(shape, dtype="int"):
     # initialize the list of (x, y)-coordinates
-    coordinates = np.zeros((68, 2), dtype=dtype)
 
     # loop over the 68 facial landmarks and convert them
+    coordinates = np.zeros((68, 2), dtype=dtype)
     # to a 2-tuple of (x, y)-coordinates
     for i in range(0, 68):
         coordinates[i] = (shape.part(i).x, shape.part(i).y)
